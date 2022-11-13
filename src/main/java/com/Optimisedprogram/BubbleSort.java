26669e71da8b22322545b0d6901e3b783ec6c6a9
madhavichaudhary0812 <madhavichaudhary0812@gmail.com>
2022-11-14 00:57:46 +0530

    new program add

---
+++ src/main/java/com/Optimisedprogram/BubbleSort.java
@@ -0,0 +1,40 @@
+package com.Optimisedprogram;
+
+public class BubbleSort {
+
+    /* Bubble Sort - 
+    *
+    *
+    * */
+
+    public static int[] bubbleSortArray(int arr[])
+    {
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-i-1; j++)
+            {
+                if(arr[j] > arr[j+1])
+                {
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    public static void display(int arr[])
+    {
+        for (int i = 0; i < arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {100,15,234,11,567,8245,1000};
+        display(bubbleSortArray(arr));
+    }
+}
